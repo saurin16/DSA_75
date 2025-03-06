a29c68a27c36243ae93db15b9b6682a73a17f17c
saurin16 <patelsaurin2002@gmail.com>
2025-03-06 02:40:13 -0600

    first commit

---
+++ TREE/Height_of_binary_tree.java
@@ -0,0 +1,34 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class Height_of_binary_tree {
+
+    public static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println("Height of the binary tree is: " + height(root));
+    }
+}
