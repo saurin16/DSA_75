5e90b7f70b10c7eac3e5db7e718b9a6c6055feab
saurin16 <patelsaurin2002@gmail.com>
2025-03-09 03:26:14 -0500

    TREE

---
+++ TREE/Traversing_tree.java
@@ -0,0 +1,54 @@
+package DSA_75.TREE;
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class Traversing_tree {
+    private static void preorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    private static  void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    private  static void postorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println("Preorder traversal of the binary tree is: ");
+        preorder(root);
+        System.out.println("\nInorder traversal of the binary tree is: ");
+        inorder(root);
+        System.out.println("\nPostorder traversal of the binary tree is: ");
+        postorder(root);
+    }
+}
